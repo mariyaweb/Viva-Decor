3ca0ed349d31a48c26b6f23097487f08126b986e
github-mariyaweb <96mariyask@gmail.com>
2024-11-24 23:03:08 +0300

    feat: add animation and ancor links

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+/* Drag and drop */
+
 const carousel = document.querySelector('.cards__wrapper');
 const arrowsBtns = document.querySelectorAll('.btn-control');
 let firstCardWidth = document.querySelector('.feedback__card').offsetWidth + 20;
 
 window.addEventListener('resize', function () {
-  firstCardWidth = document.querySelector('.feedback__card').offsetWidth;
-  console.log(firstCardWidth);
+  firstCardWidth = document.querySelector('.feedback__card').offsetWidth + 20;
 });
 
 let isDragging = false,
@@ -15,8 +16,7 @@ arrowsBtns.forEach(btn => {
   btn.addEventListener('click', () => {
     carousel.scrollLeft += btn.id === 'left' ? -firstCardWidth : firstCardWidth;
   });
-
-})
+});
 
 const dragStart = (e) => {
   isDragging = true;
@@ -38,3 +38,67 @@ const dragging = (e) => {
 carousel.addEventListener('mousedown', dragStart);
 carousel.addEventListener('mousemove', dragging);
 document.addEventListener('mouseup', dragStop);
+
+/* Swipe */
+
+document.querySelector('.cards__wrapper').addEventListener('touchstart', (event) => {
+  const startX = event.touches[0].clientX;
+
+  document.addEventListener('touchend', (event) => {
+    const endX = event.changedTouches[0].clientX;
+    const deltaX = endX - startX;
+      if (deltaX > 0) {
+        carousel.scrollBy(-firstCardWidth, 0); 
+      } else {
+        carousel.scrollBy(firstCardWidth, 0);
+      }
+  });
+});
+
+
+/* Animation */
+
+const benefits = document.querySelector('.benefits__items');
+const benefitsValues = [400, 600, 100];
+let isShowBenefits = false;
+
+function playAnimation(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('element-show');
+    }
+  });
+  if (!isShowBenefits && benefits.classList.contains('element-show')) {
+    Array.from(document.querySelectorAll('.benefits__value')).forEach((el, index) => {
+      playAnimationIncrement(el, benefitsValues[index]);
+    });
+    isShowBenefits = true;
+  }
+}
+
+function playAnimationIncrement(el, maxValue) {
+  let initNum = 0;
+  const time = 4000;
+  const interval = time / maxValue;
+  let timer = setInterval(() => {
+    initNum += 1;
+    if (initNum === maxValue) {
+      clearInterval(timer);
+    }
+    el.innerHTML = initNum + '+';
+  }, interval)
+
+}
+
+let scrollObserver = new IntersectionObserver(playAnimation, {
+  threshold: [0.2]
+});
+
+document.querySelectorAll('.element-animation').forEach(section => {
+  scrollObserver.observe(section);
+});
+
+/* Btn listeners */
+document.querySelector('.main__arrow-btn').addEventListener('click', () => {
+  window.location.href = '#services';
+})
